611711f26ddb8b9783142a47dde0b427b13483b2
Avisa-SEI <aap571420@gmail.com>
2021-06-07 16:35:57 -0700

    add: full CRUD

---
+++ server.js
@@ -0,0 +1,110 @@
+
+//___________________
+//Dependencies
+//___________________
+const mongoose = require('mongoose')
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
+const db = mongoose.connection
+const path = require('path')
+require('dotenv').config()
+
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+
+const PORT = process.env.PORT || 3000
+
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+//___________________
+//Middleware
+//___________________
+
+//use public folder for static assets
+app.use(express.static('public'))
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+//use method override
+app.use(methodOverride('_method'))
+
+// Perform CRUD on our model
+// require the model
+const Product = require('./models/product')
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongod connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongod disconnected'));
+
+//___________________
+// Routes
+//___________________
+//localhost:3000
+app.get('/' , (req, res) => {
+    res.send('Hello World!');
+  });
+
+//   Setup our Routers/Controller code
+app.get('/products', (req, res) => {
+  Product.find({}, (error, foundProduct) => {
+      res.send(foundProduct)
+  })
+})
+
+// Index
+// Show
+app.get('/products/:id', (req, res) => {
+	Product.findById(req.params.id, (error, foundProduct) => {
+		res.send(foundProduct);
+	});
+});
+
+// DELETE
+app.delete('/products/:id', (req, res) => {
+    Product.findByIdAndDelete(req.params.id, (err, deletedProduct) => {
+        res.send(deletedProduct)
+    })
+})
+
+
+// UPDATE 
+app.put('/products/:id', (req, res) => {
+    Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true},
+        (error, updatedProduct) => {
+            res.send(updatedProduct)
+        }
+    )
+})
+// Create Route/Controller
+app.post('/products', (req, res) => {
+    Product.create(req.body, function(err, newProduct) {
+        res.send(newProduct)
+    })
+  })
+
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log('express is listening on:', PORT));
